fea1a3d9c76d4a46eb89212a265a843cec639580
Wesley DuSell <dusellfam@gmail.com>
2023-03-11 20:39:50 -0500

    Convert Dashboard to standalone more

--- apps/admin/src/app/pages/dashboard/dynamic-grid-list/dynamic-grid-list.component.ts
+++ apps/admin/src/app/pages/dashboard/dynamic-grid-list/dynamic-grid-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -20,8 +20,49 @@ import { CommonModule } from '@angular/common';
     LayoutModule
   ],
   selector: 'ngx-dynamic-grid-list',
-  templateUrl: './dynamic-grid-list.component.html',
-  styleUrls: ['./dynamic-grid-list.component.scss']
+  template: `
+    <div class="grid-container">
+      <h1 class="mat-h1">Dashboard</h1>
+      <mat-grid-list cols="2" rowHeight="350px">
+        <mat-grid-tile *ngFor="let card of cards | async" [colspan]="card.cols" [rowspan]="card.rows">
+          <mat-card class="dashboard-card">
+            <mat-card-header>
+              <mat-card-title>
+                {{card.title}}
+                <button mat-icon-button class="more-button" [matMenuTriggerFor]="menu" aria-label="Toggle menu">
+                  <mat-icon>more_vert</mat-icon>
+                </button>
+                <mat-menu #menu="matMenu" xPosition="before">
+                  <button mat-menu-item>Expand</button>
+                  <button mat-menu-item>Remove</button>
+                </mat-menu>
+              </mat-card-title>
+            </mat-card-header>
+            <mat-card-content class="dashboard-card-content">
+              <div>Card Content Here</div>
+            </mat-card-content>
+          </mat-card>
+        </mat-grid-tile>
+      </mat-grid-list>
+    </div>
+  `,
+  styles: [`
+    .grid-container { margin: 20px; }
+    .dashboard-card {
+      position: absolute;
+      top: 15px;
+      left: 15px;
+      right: 15px;
+      bottom: 15px;
+    }
+    .more-button {
+      position: absolute;
+      top: 5px;
+      right: 10px;
+    }
+    .dashboard-card-content { text-align: center; }
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DynamicGridListComponent {
   /** Based on the screen size, switch from standard to one column per row */
