0ef59f06d41520215392e42e9ce4990854a574d9
Wesley DuSell <dusellfam@gmail.com>
2023-03-18 12:18:17 -0400

    Changing port number for api to be served on because of Grafana

--- apps/workshop-api/src/main.ts
+++ apps/workshop-api/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4100);
 }
 bootstrap();
