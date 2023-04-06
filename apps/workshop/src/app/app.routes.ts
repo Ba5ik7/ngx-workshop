1d6cc86102f69c938a70c8a3c1060061e9bab897
Wesley DuSell <dusellfam@gmail.com>
2023-04-05 20:59:14 -0400

    Getting ready for stand alone convertion pass

--- apps/workshop/src/app/app.routes.ts
+++ apps/workshop/src/app/app.routes.ts
@@ -27,17 +27,23 @@ export const appRoutes: Routes = [
     pathMatch: 'full',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
-  // Navbar links to redirect to workshop-sidenav module
-  { path: 'angular', pathMatch: 'full', redirectTo: '/angular/categories' },
-  { path: 'nestjs', pathMatch: 'full', redirectTo: '/nestjs/categories' },
-  { path: 'rxjs', pathMatch: 'full', redirectTo: '/rxjs/categories' },
-  { path: 'account', loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule) },
-  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) },
-  // This ne`eds to before the workshop-sidenav route becuase missing routes in that module will redirect to this 404 route
-  { path: '404', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule)
+  },
   {
     path: ':section',
     loadChildren: () => import('./pages/workshop-sidenav/workshop-sidenav.module').then(m => m.WorkshopSidenavModule)
   },
-  { path: '**', redirectTo: '/404' },
+  { 
+    path: '404',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  },
+  {
+    path: '**', redirectTo: '/404'
+  },
 ];
