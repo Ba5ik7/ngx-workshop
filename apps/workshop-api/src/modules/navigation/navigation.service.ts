79c173de560b5771f608caca621246893abcb33a
Wesley DuSell <dusellfam@gmail.com>
2023-04-26 23:05:03 -0400

    Remove unneeded imports

--- apps/workshop-api/src/modules/navigation/navigation.service.ts
+++ apps/workshop-api/src/modules/navigation/navigation.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import { IWorkshopDocument } from '../../interfaces/workshop-document.interface';
 import { WorkshopDocumentService } from '../workshop-document/workshop-document.service';
 import {
